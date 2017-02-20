b724ed5ca3a9ff1726cf670bfbef6adb261a4a78
jelles-immalle <jelle.swaelen@immalle.be>
2017-02-20 09:59:06 +0100

    Zesde oefening afgewerkt

--- 05_operators.php
+++ 05_operators.php
@@ -1,10 +1,14 @@
 <?php
 
 // Bereken de rest bij gehele deling van 10 door 3.
-
+$varDelingMetRest = 10 % 3;
+echo $varDelingMetRest;
+echo "<br>";
 
 // Bereken de 5e macht van 2.
-
+$machtBerekening = pow (2, 5);
+echo $machtBerekening;
+echo "<br>";
 
 
 $var_a = "123";
@@ -12,11 +16,13 @@ $var_b = 123;
 
 // Vervang de true in de if-voorwaarden zodat het klopt wat er staat:
 
-if(true) {
+if($var_a == $var_b) {
+    echo "<br>";
     echo '$var_a en $var_b zijn aan elkaar gelijk maar zijn niet van hetzelfde type.';
 }
 
-if(true) {
+if($var_a === $var_b) {
+    echo "<br>";
     echo '$var_a en $var_b zijn aan elkaar gelijk en van hetzelfde type.';
 }
 
