45805bbc2bef865848b9f32e13bc72630aa5054e
jelles-immalle <jelle.swaelen@immalle.be>
2017-02-20 09:34:28 +0100

    Derde oefening afgewerkt

--- 02_case_sensitivity.php
+++ 02_case_sensitivity.php
@@ -19,6 +19,13 @@ if($val == null) {
     echo "$val is not defined.";
 }
 
+echo "<br>";
+//Onderstaande code print de variabele in de string ook af, ipv "$waarde" af te printen.
+echo "De waarde van $waarde is ".$waarde;
+//Terwijl met single quotes de waarde van de variabele niet wordt afgeprint.
+echo "<br>";
+echo 'De waarde van $waarde is '.$waarde;
+
 // Print de string "De waarde van $waarde is 8." door maar 1 keer gebruik te maken v.d. concatentie-operator en de juiste soort quotes: single (') of double (").
 
 
