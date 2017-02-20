c0d6c7e29842f50883a74e904910c0a0cc615c59
jelles-immalle <jelle.swaelen@immalle.be>
2017-02-20 09:49:15 +0100

    Vierde oefening afgewerkt

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -2,10 +2,13 @@
 
 $zin = "Binnenkort is het weeral lente";
 
+echo str_word_count("$zin");
 // Print af hoeveel woorden $zin bevat.
 
-
+$position = strpos ($zin, "lente");
+echo "<br>";
+$nieuweZin = substr_replace ( $zin, "zomer", $position);
+echo $nieuweZin;
 // Vervang in $zin "lente" door "zomer" en druk af.
-
 
 ?>
