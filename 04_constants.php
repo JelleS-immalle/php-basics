a883d5c2b7e14588e0b15dc98560da14152f69e6
jelles-immalle <jelle.swaelen@immalle.be>
2017-02-20 09:51:15 +0100

    Vijfde oefening afgewerkt

--- 04_constants.php
+++ 04_constants.php
@@ -1,8 +1,14 @@
 <?php
 
 define("USERNAME", "joske");
+define("PASSWORD", "testwachtwoord");
+define("SERVERNAME", "JosLand");
 
 echo USERNAME;
+echo "<br>";
+echo PASSWORD;
+echo "<br>";
+echo SERVERNAME;
 
 // definieer ook een constante PASSWORD en SERVERNAME
 
