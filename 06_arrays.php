b905f715bf5fccf67696eb8fff6403ec1135433a
jelles-immalle <jelle.swaelen@immalle.be>
2017-02-20 10:03:12 +0100

    Zevende oefening half afgewerkt

--- 06_arrays.php
+++ 06_arrays.php
@@ -1,10 +1,13 @@
 <?php
 
 // Maak een Array met 3 namen van medeleerlingen.
-
+$arrayNamen = array(Jos, Jef, Jim);
 
 // Itereer er over met een for-loop. Print ze af in een HTML ongenummerde lijst.
-
+foreach(array_expresssion as $naam){
+    $teller = $teller++;
+    echo $arrayNamen[$teller];
+}
 
 // Itereer er over met een foreach-loop. Print ze af in HTML genummerde lijst.
 
