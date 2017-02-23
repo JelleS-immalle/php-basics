fd3ed9f8c324b0b206170c6770baea1b9cf4de67
jelles-immalle <jelle.swaelen@immalle.be>
2017-02-23 15:09:58 +0100

    Achtste oefening afgewerkt

--- 07_switch_scrabble.php
+++ 07_switch_scrabble.php
@@ -3,6 +3,46 @@
 $woord = "quotering";
 $punten = 0;
 
+for($i = 0; $i <strlen($woord); $i++){
+	switch(strtoupper($woord[$i])){
+		case "Q":
+			$punten += 10;
+			echo "Goedzo, je krijgt 10 punten!";
+			break;
+		case "U":
+			$punten += 4;
+			echo "Goedzo, je krijgt 4 punten!";
+			break;
+		case "O":
+			$punten += 1;
+			echo "Goedzo, je krijgt 1 punt!";
+			break;
+		case "T":
+			$punten += 2;
+			echo "Goedzo, je krijgt 2 punten!";
+			break;
+		case "E":
+			$punten += 1;
+			echo "Goedzo, je krijgt 1 punt!";
+			break;
+		case "R":
+			$punten += 2;
+			echo "Goedzo, je krijgt 2 punten!";
+			break;
+		case "I":
+			$punten += 1;
+			echo "Goedzo, je krijgt 1 punt!";
+			break;
+		case "N":
+			$punten += 1;
+			echo "Goedzo, je krijgt 1 punt!";
+			break;
+	}
+}
+
+echo "<br>";
+echo "Je hebt in totaal: $punten punten.";
+echo "<br>";
 // gebruik een switch-case-statement binnen een for-loop om
 // te itereren over een woord en de Scrabble-punten ervan te berekenen
 
